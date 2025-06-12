95254034089f9aab9e006604582303ca2313b65c
Stefan Keller <aegiskeller@gmail.com>
2025-06-12 15:45:55 +1000

    tweak

--- ArduSafeMon/ArduSafeMon_R4wifi_weather/getweather.py
+++ ArduSafeMon/ArduSafeMon_R4wifi_weather/getweather.py
@@ -53,8 +53,9 @@ def periodic_weather_update(api_key, city, interval=600):
         time.sleep(interval)
 
 def main():
-    api_key = <your api>
-    city = <your city>
+    api_key = API_KEY
+    city = CITY
+    # Initial weather data fetch
     weather_data = get_weather(api_key, city)
     if weather_data:
         save_weather_data(weather_data)
