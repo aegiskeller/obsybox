cd191897c873a4685d6be0eb8a4f14d844e04250
Stefan Keller <aegiskeller@gmail.com>
2025-06-30 12:46:48 +1000

    fix name

--- systemMonitoring/EquipmentMonitoring/TelescopeMonitoring.py
+++ systemMonitoring/EquipmentMonitoring/TelescopeMonitoring.py
@@ -12,7 +12,7 @@ sys.path.append(r"C:\Program Files (x86)\Common Files\ASCOM\Platform")
 
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
-MQTT_TOPIC_MOUNT = "obsybox/telescope/position"
+MQTT_TOPIC_MOUNT = "obsybox/telescope/status"
 MQTT_TOPIC_CAMERA = "obsybox/camera/status"
 MQTT_TOPIC_DOME = "obsybox/dome/status"
 MQTT_TOPIC_FILTERWHEEL = "obsybox/filterwheel/status"
